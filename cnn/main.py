5cec67f98b0ffe94edd2d226d1e6a6db88cf1740
Gabriel Noya <gabrielnoya95@gmail.com>
2022-02-23 20:19:17 -0400

    CNN U-net training and learning with 1 image

--- cnn/main.py
+++ cnn/main.py
@@ -8,6 +8,7 @@ from datasets import UNETDataset, DataLoaderCreator
 from models import UNet
 from utils import np_utils, data_handler
 from training import train, get_data
+from torchvision.utils import save_image
 
 # ---------- Opening parameters
 with open(os.path.dirname(__file__) + 'parameters.json') as path_file:
@@ -21,7 +22,7 @@ dataloader_creator = DataLoaderCreator(params)
 training_loader, validation_loader = dataloader_creator.get_loaders()
 
 # ---------- Models
-unet = UNet(n_channels=3, n_classes=3, bilinear=False).to(device)
+unet = UNet(n_channels=3, n_classes=3, bilinear=False, learning_rate=params['unet']['learning_rate'], adam_b1=params['unet']['beta1'], adam_b2=params['unet']['beta2']).to(device)
 
 loss_function = torch.nn.BCELoss()
 handler = data_handler.DataHandler(True, None)
@@ -34,6 +35,9 @@ for epoch in range(params['epochs']):
         image, gt = get_data(data, device)
         y_hat, loss = train(unet, image, gt, loss_function, device)
         handler.append_train_loss(loss)
+
+        if epoch % 10 == 0:
+            save_image(y_hat, "images/{}.png".format(epoch), nrow=1)
 
     handler.epoch_end(epoch, unet.lr)
 

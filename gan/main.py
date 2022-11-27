654439f8a64d66224cc3ba1f1dde53b8f7f5fa12
cappelletto <cappelletto@gmail.com>
2022-11-27 12:02:45 +0000

    Format test for GAN main and training modules [black]

--- gan/main.py
+++ gan/main.py
@@ -11,11 +11,11 @@ from utils import np_utils, data_handler
 from training import train, validate_d, get_data
 
 # ---------- Opening parameters
-with open(os.path.dirname(__file__) + 'parameters.json') as path_file:
+with open(os.path.dirname(__file__) + "parameters.json") as path_file:
     params = json.load(path_file)
 
 # ---------- Checking for CUDA
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # ---------- Dataset
 dataloader_creator = DataLoaderCreator(params)
@@ -29,13 +29,15 @@ loss_function = torch.nn.BCELoss()
 handler = data_handler.DataHandler(True, True)
 
 # ---------- Training epochs
-for epoch in range(params['epochs']):
+for epoch in range(params["epochs"]):
     # ------------------- Training the GAN --------------------- #
     for i, data in enumerate(training_loader, 0):
         # ------ Get the data from the data_loader
         in_air, underwater = get_data(data, device)
-        
-        fake_underwater, g_loss, d_loss = train(generator, discriminator, in_air, underwater, loss_function, device)
+
+        fake_underwater, g_loss, d_loss = train(
+            generator, discriminator, in_air, underwater, loss_function, device
+        )
         handler.append_train_loss(d_loss.item())
 
     # ------------------- Validatin the GAN--------------------- #
@@ -43,7 +45,9 @@ for epoch in range(params['epochs']):
         # ------ Get the data from the data_loader
         in_air, underwater = get_data(data, device)
 
-        d_loss = validate_d(generator, discriminator, in_air, underwater, loss_function, device)
+        d_loss = validate_d(
+            generator, discriminator, in_air, underwater, loss_function, device
+        )
         handler.append_valid_loss(d_loss.item())
 
     handler.epoch_end(epoch, 0)
--- gan/training.py
+++ gan/training.py
@@ -1,14 +1,23 @@
 import torch
 import numpy as np
 
+
 def train(generator, discriminator, in_air, underwater, loss_function, device):
     # ------- Training mode
     generator.train()
     discriminator.train()
 
     # ------ Create valid and fake ground truth
-    valid = torch.tensor(np.ones((in_air.shape[0], 1)), requires_grad=False).float().to(device)
-    fake = torch.tensor(np.zeros((in_air.shape[0], 1)), requires_grad=False).float().to(device)
+    valid = (
+        torch.tensor(np.ones((in_air.shape[0], 1)), requires_grad=False)
+        .float()
+        .to(device)
+    )
+    fake = (
+        torch.tensor(np.zeros((in_air.shape[0], 1)), requires_grad=False)
+        .float()
+        .to(device)
+    )
 
     # ---------------- Train generator ---------------- #
     generator.optimizer.zero_grad()
@@ -22,7 +31,6 @@ def train(generator, discriminator, in_air, underwater, loss_function, device):
     # ------ Backpropagate the Generator
     g_loss = generator.backpropagate(fake_prediction, valid)
 
-
     # -------------- Train discriminator -------------- #
     discriminator.optimizer.zero_grad()
 
@@ -34,7 +42,6 @@ def train(generator, discriminator, in_air, underwater, loss_function, device):
     # ------ Backpropagate the Discriminator
     d_loss.backward()
     discriminator.optimizer.step()
-    
 
     return fake_underwater, g_loss, d_loss
 
@@ -43,10 +50,18 @@ def validate_d(generator, discriminator, in_air, underwater, loss_function, devi
     with torch.no_grad():
         generator.eval()
         discriminator.eval()
-    
+
         # ------ Create valid and fake ground truth
-        valid = torch.tensor(np.ones((in_air.shape[0], 1)), requires_grad=False).float().to(device)
-        fake = torch.tensor(np.zeros((in_air.shape[0], 1)), requires_grad=False).float().to(device)
+        valid = (
+            torch.tensor(np.ones((in_air.shape[0], 1)), requires_grad=False)
+            .float()
+            .to(device)
+        )
+        fake = (
+            torch.tensor(np.zeros((in_air.shape[0], 1)), requires_grad=False)
+            .float()
+            .to(device)
+        )
 
         # ------ Generate fake underwater images
         fake_underwater = generator(in_air)
@@ -55,12 +70,12 @@ def validate_d(generator, discriminator, in_air, underwater, loss_function, devi
         real_loss = loss_function(discriminator(underwater), valid)
         fake_loss = loss_function(discriminator(fake_underwater.detach()), fake)
         d_loss = (real_loss + fake_loss) / 2
-    
+
         return d_loss
 
 
 def get_data(data, device):
-    in_air = data['in_air'].to(device)
-    underwater = data['underwater'].to(device)
+    in_air = data["in_air"].to(device)
+    underwater = data["underwater"].to(device)
 
     return in_air, underwater

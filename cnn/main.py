5ce4f99323455f446f9143ec57fa4561bdbab72e
cappelletto <cappelletto@gmail.com>
2022-11-27 12:00:03 +0000

    Format test for CNN main and training modules [black]

--- cnn/main.py
+++ cnn/main.py
@@ -11,24 +11,31 @@ from training import train, get_data
 from torchvision.utils import save_image
 
 # ---------- Opening parameters
-with open(os.path.dirname(__file__) + 'parameters.json') as path_file:
+with open(os.path.dirname(__file__) + "parameters.json") as path_file:
     params = json.load(path_file)
 
 # ---------- Checking for CUDA
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # ---------- Dataset
 dataloader_creator = DataLoaderCreator(params)
 training_loader, validation_loader = dataloader_creator.get_loaders()
 
 # ---------- Models
-unet = UNet(n_channels=3, n_classes=3, bilinear=False, learning_rate=params['unet']['learning_rate'], adam_b1=params['unet']['beta1'], adam_b2=params['unet']['beta2']).to(device)
+unet = UNet(
+    n_channels=3,
+    n_classes=3,
+    bilinear=False,
+    learning_rate=params["unet"]["learning_rate"],
+    adam_b1=params["unet"]["beta1"],
+    adam_b2=params["unet"]["beta2"],
+).to(device)
 
 loss_function = torch.nn.BCELoss()
 handler = data_handler.DataHandler(True, None)
 
 # ---------- Training epochs
-for epoch in range(params['epochs']):
+for epoch in range(params["epochs"]):
     # ------------------- Training the GAN --------------------- #
     for i, data in enumerate(training_loader, 0):
         # ------ Get the data from the data_loader
--- cnn/training.py
+++ cnn/training.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 
+
 def train(model, image, gt, loss_function, device):
     # ------- Training mode
     model.train()
@@ -13,8 +14,9 @@ def train(model, image, gt, loss_function, device):
 
     # ------ Backpropagate the UNet
     loss = model.backpropagate(y_hat, gt)
-    
+
     return y_hat, loss
+
 
 def get_data(data, device):
     image, gt = data

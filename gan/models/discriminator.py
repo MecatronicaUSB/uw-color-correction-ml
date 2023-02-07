Bug fix in the discriminator backpropagation

gan/models/discriminator.py
@@ -65,14 +65,11 @@ class Discriminator(nn.Module):
         # ------ Calculate real and fake images discriminator loss
         real_loss = self.loss_function(self(underwater), valid_gt)
         fake_loss = self.loss_function(self(fake_underwater.detach()), fake_gt)
+        d_loss = (real_loss + fake_loss) / 2
         
         # ------ Backpropagate discriminator
         if training:
-            real_loss.backward()
+            d_loss.backward()
             self.optimizer.step()
-            fake_loss.backward()
-            self.optimizer.step()
-
-        d_loss = (real_loss + fake_loss) / 2
 
         return d_loss.item()